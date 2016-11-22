2ba8bfa147bc7d889e5ae1b4476b6a3bc903f5f2
ayane-tobiyama <ree.go.pef7v@gmail.com>
2016-11-22 14:54:31 +0900

    課題1-4

---
+++ 005.オブジェクト指向１/kadai3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ayanet
+ */
+public class kadai3{
+    
+    public String Maker = "";
+    public String Color = "";
+    
+    public void setkadai3(String m,String c){
+        Color = c;
+        Maker = m;
+    }
+    
+    public void showColor(){
+        System.out.println(Color);
+    }
+    
+    public void showMaker(){
+        System.out.println(Maker);
+    }
+        
+    
+}
---
+++ 005.オブジェクト指向１/kadai4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ayanet
+ */
+public class kadai4 extends kadai3{
+      
+        public static void main(String[] args){
+   
+            kadai3 iphone = new kadai3();
+            iphone.setkadai3("apple","gold");
+            iphone.showColor();
+            iphone.showMaker();
+            
+            kadai3 kesigomu = new kadai3();
+            kesigomu.setkadai3("nomo","white");
+            kesigomu.showColor();
+            kesigomu.showMaker();
+            
+        }
+    
+}
